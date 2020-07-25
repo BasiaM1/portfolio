cbded29d30a776f28ef0e8f22e4628d0726f2f2e
BasiaM1 <b.murzynowska@wp.pl>
2020-07-26 01:33:43 +0200

    Add scroll on clik in menu

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$('.me').on('click', function () {
+    $('body, html').animate({
+        scrollTop: $('.about').offset().top
+    }, 500)
+})
+
+$('.projects').on('click', function () {
+    $('body, html').animate({
+        scrollTop: $('.portfolio').offset().top
+    }, 500)
+})
+
+$('.contact').on('click', function () {
+    $('body, html').animate({
+        scrollTop: $('.footer-list').offset().top
+    }, 500)
+})
