3ea3ca2e7b343f3be3a8d6d2d5873b379d386d40
BasiaM1 <b.murzynowska@wp.pl>
2020-07-26 13:15:25 +0200

    Add scroll on home

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+$('.top').on('click', function () {
+    $('body, html').animate({
+        scrollTop: $('.splash').offset().top
+    }, 500)
+})
+
+
 $('.me').on('click', function () {
     $('body, html').animate({
         scrollTop: $('.about').offset().top
